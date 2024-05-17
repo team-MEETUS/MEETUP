82d2541c4d00c0eb78d86e16322be6457ac486a4
chaeyoon <chaeyoon010809@gmail.com>
2024-05-17 17:56:20 +0900

    first commit

---
+++ src/main/java/kr/co/meetup/web/main/MainController.java
@@ -0,0 +1,37 @@
+package kr.co.meetup.web.main;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.co.meetup.web.main.Action;
+
+@SuppressWarnings("serial")
+@WebServlet("/meetup.do")
+public class MainController extends HttpServlet {
+	
+	private void doProcess(HttpServletRequest req, HttpServletResponse resp) throws UnsupportedEncodingException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
+		
+		// cmd 파라미터 값 가져오기 
+		String cmd = req.getParameter("cmd");
+		String url = "";
+		Action action = null;
+		
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+	
+}
