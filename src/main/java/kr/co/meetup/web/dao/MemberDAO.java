2e86d1fb5ff2c2aa3ca4a9373e9f1cfb24659fe6
HS <lhs991117@gmail.com>
2024-05-21 00:26:34 +0900

    feat: member 파일 생성

--- src/main/java/kr/co/meetup/web/dao/MemberDAO.java
+++ src/main/java/kr/co/meetup/web/dao/MemberDAO.java
@@ -1,5 +1,26 @@
 package kr.co.meetup.web.dao;
 
-public class MemberDAO {
+import java.io.IOException;
+import java.io.Reader;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MemberDAO {
+	private SqlSessionFactory factory;
+	
+	public MemberDAO() {
+		try {
+			Reader r = Resources.getResourceAsReader("config/SqlMapConfig.xml");
+			SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+			factory = builder.build(r);
+			r.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
