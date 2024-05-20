9e36dad7d46f01916de50fd86cab00207d8ee8ea
HS <lhs991117@gmail.com>
2024-05-20 17:29:51 +0900

    feat: memberVO 생성

--- src/main/java/kr/co/meetup/web/vo/MemberVO.java
+++ src/main/java/kr/co/meetup/web/vo/MemberVO.java
@@ -1,5 +1,25 @@
 package kr.co.meetup.web.vo;
 
-public class MemberVO {
+import java.sql.Date;
+import java.sql.Timestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberVO {
+	private int memberNo;
+	private int geoCode;
+	private String memberPhone;
+	private String memberPw;
+	private String memberNickname;
+	private int memberStatus;
+	private String memberOriginalImg;
+	private String memberSaveImg;
+	private Date memberDeadDate;
+	private Timestamp createdAt;
+	private Timestamp updatedAt;
 }
