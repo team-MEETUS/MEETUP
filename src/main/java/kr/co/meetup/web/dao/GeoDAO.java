065c19502217ac048511a560c0084ae18400522f
HS <lhs991117@gmail.com>
2024-05-21 17:53:13 +0900

    feat: geo 파일 수정

--- src/main/java/kr/co/meetup/web/dao/GeoDAO.java
+++ src/main/java/kr/co/meetup/web/dao/GeoDAO.java
@@ -2,10 +2,15 @@ package kr.co.meetup.web.dao;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.HashMap;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import kr.co.meetup.web.vo.GeoVO;
 
 public class GeoDAO {
 	private SqlSessionFactory factory;
@@ -19,5 +24,23 @@ public class GeoDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public List<GeoVO> selectAllGeo() {
+		SqlSession ss = factory.openSession(true);
+		List<GeoVO> list = ss.selectList("kr.co.meetup.geo.selectAllGeo");
+		return list;
+	}
+	
+	public int selectOneGeoByCity(String geoCity, String geoDistrict) {
+		SqlSession ss = factory.openSession(true);
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("geoCity", geoCity);
+		map.put("geoDistrict", geoDistrict);
+		
+		int geoCode = ss.selectOne("kr.co.meetup.geo.selectOneGeoByCity", map);
+		
+		ss.close();
+		return geoCode;
 	}
 }
