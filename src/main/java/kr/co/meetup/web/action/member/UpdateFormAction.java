0feb9c218cebd8791d0346ff8ba7816f1b33c0f0
HS <lhs991117@gmail.com>
2024-05-23 17:48:14 +0900

    feat: member 회원가입

---
+++ src/main/java/kr/co/meetup/web/action/member/UpdateFormAction.java
@@ -0,0 +1,30 @@
+package kr.co.meetup.web.action.member;
+
+import java.util.List;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import kr.co.meetup.web.action.Action;
+import kr.co.meetup.web.dao.GeoDAO;
+import kr.co.meetup.web.vo.GeoVO;
+import kr.co.meetup.web.vo.MemberVO;
+
+public class UpdateFormAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse resp) {
+		HttpSession session = req.getSession();
+		MemberVO mvo = (MemberVO) session.getAttribute("MemberVO");
+		if(mvo != null) {
+			GeoDAO dao = new GeoDAO();
+			GeoVO gvo = dao.selectOneGeoCityGeoDistrictByGeoCode(mvo.getGeoCode());
+			List<String> geoCities = dao.selectAllGeoCity();
+			
+			req.setAttribute("GeoVO", gvo);
+			req.setAttribute("geoCities", geoCities);
+		}
+		return "member/myPage.jsp";
+	}
+
+}
