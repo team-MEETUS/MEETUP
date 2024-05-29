ddc3e71b3955b688bd7def286069d20598c42ab0
HS <lhs991117@gmail.com>
2024-05-29 19:48:06 +0900

    design: member 정보 페이지 작업

--- src/main/java/kr/co/meetup/web/action/member/MyMenuAction.java
+++ src/main/java/kr/co/meetup/web/action/member/MyMenuAction.java
@@ -1,10 +1,14 @@
 package kr.co.meetup.web.action.member;
 
+import java.util.List;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import kr.co.meetup.web.action.Action;
+import kr.co.meetup.web.dao.CrewDAO;
 import kr.co.meetup.web.dao.GeoDAO;
+import kr.co.meetup.web.vo.CrewVO;
 import kr.co.meetup.web.vo.GeoVO;
 import kr.co.meetup.web.vo.MemberVO;
 
@@ -20,8 +24,15 @@ public class MyMenuAction implements Action {
 		if(vo != null) {
 			GeoDAO gdao = new GeoDAO();
 			GeoVO gvo = gdao.selectOneGeoCityGeoDistrictByGeoCode(vo.getGeoCode());
+			
+			CrewDAO cdao = new CrewDAO();
+			List<CrewVO> loginMemberCrewList = cdao.selectAllCrewByMember(vo.getMemberNo());
+			List<CrewVO> loginMemberLikeCrewList = cdao.selectAllCrewLikeByMember(vo.getMemberNo());
+			
 			url = "member/myMenu.jsp";
 			req.setAttribute("GeoVO", gvo);
+			req.setAttribute("loginMemberCrewList", loginMemberCrewList);
+			req.setAttribute("loginMemberLikeCrewList", loginMemberLikeCrewList);
 		} 
 		// 그렇지 않다면 로그인 페이지
 		else {
